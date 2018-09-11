3462a989a4e0f2c05b69ff26bdc19580700c3d0f
Baz <jennigoldstein@gmail.com>
2018-09-11 15:28:32 -0500

    updated routes

--- public/js/class.js
+++ public/js/class.js
@@ -20,108 +20,81 @@ $(document).ready(function () {
                 url: "api/class/" + id,
                 type: "GET"
             });
-        }   
-    };
-
-    // refreshExamples gets new examples from the db and repopulates the list
-    var refreshClasses = function () {
-        API.getClasses().then(function (data) {
-            classes = data.map(function (classInfo) {
-                classDiv = $("<div>").text(classInfo.title)
-                    .addClass(classInfo.id);
-
-                var classInfoList = $("<li>")
-                    .attr({
-                        class: "list-group-item",
-                        "data-id": classInfo.id
-                        //do this for each item per class
-                    })
-                    .append(classDiv);
-
-                var selectClass = $("<button>")
-                    .addClass("btn selectClass")
-                    .text("Learn More");
-
-                classInfoList.append(selectClass);
-
-                return classInfoList;
+        },
+        addStudent: function (id) {
+            return $.ajax({
+                type: "POST",
+                url: "api/register/" + id 
             });
-
-            $("#classList").empty();
-            $("#classList").append(classes);
-        });
+        },
     };
 
-    //   handleNewClassSubmit is called whenever the create a class is clicked
-    //   Save the new example to the db and refresh the list
-
-    var handleNewClassSubmit = function (event) {
+    //this handles on click for registration
+    var handleRegistration = function (event) {
         event.preventDefault();
+        var classID = $("#regbutton").attr("data-id");
 
-        var newClass = {
-            title: $("#title-input").val().trim(),
-            description: $("#description-input").val().trim(),
-            date: $("#date-input").val().trim(),
-            starttime: $("#starttime-input").val().trim(),
-            endtime: $("#endtime-input").val().trim(),
-            location: $("#location-input").val().trim(),
-            maxStudents: $("#spaces-input").val().trim(),
-            price: $("#price-input").val().trim(),
-            photo: $("#photo-input").val().trim(),
-            category: $("#category-input").val()
-        };
-
-        if (!(
-            newClass.title && 
-            newClass.description && 
-            newClass.category && 
-            newClass.date && 
-            newClass.starttime && 
-            newClass.endtime && 
-            newClass.location && 
-            newClass.maxStudents && 
-            newClass.price && 
-            newClass.photo &&
-            newClass.category)) {
-                alert("You must enter all information!");
-                return;
-        }
-
-        API.createClass(newClass).then(function () {
-            refreshClasses();
-        });
-
-        $("#title-input").val("");
-        $("#description-input").val("");
-        $("#date-input").val("");
-        $("#starttime-input").val("");
-        $("#endtime-input").val("");
-        $("#location-input").val("");
-        $("#spaces-input").val("");
-        $("#price-input").val("");
-        $("#photo-input").val("");
-        $("#category-input").val("");
+        API.addStudent(classID)
     };
 
-    // handlemoreinfoClick is called when a class' moreinfo button is clicked
+//   handleNewClassSubmit is called whenever the create a class is clicked
+//   Save the new example to the db and refresh the list
 
-    var handleMoreInfoClick = function () {
-        var idOfClass = $(this)
-            .parent()
-            .attr("data-id");
+var handleNewClassSubmit = function (event) {
+    event.preventDefault();
 
-        API.getClass(idOfClass).then(function (data) {
-            classDisplay = data.map(function (classInfo) {
-                classDiv = $("<div>").text(classInfo.title)
-                    .addClass(classInfo.id);
-                //need to push class info to class html
-                //need to add data-id to sign-up button
-            });
-        });
+    var newClass = {
+        title: $("#title-input").val().trim(),
+        instructor: $("#instructor-input").val().trim(),
+        description: $("#description-input").val().trim(),
+        date: $("#date-input").val().trim(),
+        starttime: $("#starttime-input").val().trim(),
+        endtime: $("#endtime-input").val().trim(),
+        location: $("#location-input").val().trim(),
+        maxStudents: $("#spaces-input").val().trim(),
+        price: $("#price-input").val().trim(),
+        photo: $("#photo-input").val().trim(),
+        category: $("#category-input").val()
     };
 
-    // Add event listeners to the submit and more info buttons
-    $("#createclass-btn").on("click", handleNewClassSubmit);
-    $("#selectClass").on("click", handleMoreInfoClick);
+    if (!(
+        newClass.title &&
+        newClass.description &&
+        newClass.instructor &&
+        newClass.category &&
+        newClass.date &&
+        newClass.starttime &&
+        newClass.endtime &&
+        newClass.location &&
+        newClass.maxStudents &&
+        newClass.price &&
+        newClass.photo &&
+        newClass.category)) {
+        alert("You must enter all information!");
+        return;
+    }
 
+    API.createClass(newClass)
+
+    $("#title-input").val("");
+    $("#description-input").val("");
+    $("#instructor-input").val("");
+    $("#date-input").val("");
+    $("#starttime-input").val("");
+    $("#endtime-input").val("");
+    $("#location-input").val("");
+    $("#spaces-input").val("");
+    $("#price-input").val("");
+    $("#photo-input").val("");
+    $("#category-input").val("");
+};
+
+
+
+
+
+// Add event listeners to the submit and more info buttons
+$("#createclass-btn").on("click", handleNewClassSubmit);
+    //add onclick for registration
+    $("#regbutton").on("click", handleRegistration);
 });
