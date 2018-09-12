a8757f9cb614fa18277b96b6769ffddd4c321312
CB6 <melissa.j.womack@gmail.com>
2018-09-12 15:25:55 -0500

    wrote code to add user to class

--- routes/apiRoutes-class.js
+++ routes/apiRoutes-class.js
@@ -29,22 +29,31 @@ module.exports = function (app) {
 
   // When a user signs up for a class, take the user's ID and the class's ID and combine them on a third table in the DB
   app.post("/api/register/:id", function (req, res) {
+    
+    console.log("class id: " + req.params.id);
+    console.log("user id: " + req.body.userId);
+    var userid = req.body.userId;
     db.Class.findOne({
       where: {
         id: req.params.id
       }
+    }).catch(function (err) {
+      console.log("1st err");
+      console.log(err);
     }).then(function (classResponse) {
       db.User.findOne({
         where: {
-          id: req.user.id
+          id: userid
         }
+      }).catch(function (err) {
+        console.log("2nd err");
+        console.log(err);
       }).then(function (userResponse) {
-
         classResponse.addUser(userResponse).then(function (data) {
           res.json(data);
         });
       });
-    });
+    })
   });
   
 };
